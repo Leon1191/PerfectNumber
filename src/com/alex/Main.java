e58b884569d7356144ad802ec89897ba1381ff17
Leo <stbirb@yandex.ru>
2019-05-09 16:05:17 +0300

    Initial commit

---
+++ src/com/alex/Main.java
@@ -0,0 +1,20 @@
+package com.alex;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(isPerfectNumber(6));
+    }
+    public static boolean isPerfectNumber(int number){
+         if (number < 1){return false;}
+         int sum = 0;
+            for(int i =1; i < number; i++) {
+            if(number % i == 0) {sum += i;}
+            }
+            if (number == sum){return true;}
+            return false;
+
+    }
+
+}
+
